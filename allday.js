e3296c466b6ba41f5405d3def27802a878a6fd09
OK3 <olek3a@gmail.com>
2024-11-09 17:40:30 +0530

    обойдем вычисление progress от null?

--- allday.js
+++ allday.js
@@ -4,7 +4,7 @@ for ( var i=0;i<[cal_events].length;i++) {
 	if ( [cal_events][i].allday && [cal_events][i].progress > 0 && [cal_events][i].progress<1 ) {
 		var offset_i = [cal_events][i].begin.offset; 
 		var progress_i = [cal_events][i].progress;  
-        if ( latest_allday < offset_i || (latest_allday = offset_i && progress_i > [cal_events][current_allday].progress ) ) {
+        if ( latest_allday === null || latest_allday <= offset_i ||  (latest_allday = offset_i && progress_i > [cal_events][current_allday].progress ) ) {
             latest_allday = offset_i;
             current_allday = i;
         }
