464a4026f699d2879fa4b046af0913704d3df033
olek3 <olek3a@gmail.com>
2024-11-17 15:44:27 +0530

    cut length 18, hellip symbol

--- allday.js
+++ allday.js
@@ -1,5 +1,5 @@
 var current_allday = null;
-var cut_length = 16;
+var cut_length = 18;
 var latest_allday = [cur_evnt_full]?[cur_evnt_full].begin.offset : null;
 for ( var i=0;i<[cal_events].length;i++) {
 	if ( [cal_events][i].allday && [cal_events][i].progress > 0 && [cal_events][i].progress<1 ) {
@@ -16,7 +16,7 @@ var announce = (current_allday === null ) ? "" : [cal_events][current_allday].ti
 
 if(announce.slice(0,5).toLowerCase() == 'день ') {announce = announce.slice(5);}
 
-if (announce.length > cut_length ) { announce = announce.slice(0,cut_length) + "...";}
+if (announce.length > cut_length ) { announce = announce.slice(0,cut_length) + String.fromCharCode(8230);}
 
 return  announce ;
 
