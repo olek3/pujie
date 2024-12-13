c13560b065e70ccda567e7d6364df62ef76fd7f8
olek3 <olek3a@gmail.com>
2024-12-14 01:50:31 +0530

    по ближайшему концу события

--- allday.js
+++ allday.js
@@ -1,22 +1,23 @@
-var current_allday = null;
+var currentEvent = null;
 var cut_length = 18;
-var latest_allday = [cur_evnt_full]?[cur_evnt_full].begin.offset : null;
+var soonestFinish = [cur_evnt_full]?[cur_evnt_full].end.offset : null;
 for ( var i=0;i<[cal_events].length;i++) {
 	if ( [cal_events][i].allday && [cal_events][i].progress > 0 && [cal_events][i].progress<1 ) {
-		var offset_i = [cal_events][i].begin.offset; 
+		var offset_i = [cal_events][i].end.offset; 
 		var progress_i = [cal_events][i].progress;  
-        if ( latest_allday === null || latest_allday <= offset_i ||  (latest_allday = offset_i && progress_i > [cal_events][current_allday].progress ) ) {
-            latest_allday = offset_i;
-            current_allday = i;
+        if ( soonestFinish === null || soonestFinish <= offset_i ||  (soonestFinish = offset_i && progress_i > [cal_events][currentEvent].progress ) ) {
+            soonestFinish = offset_i;
+            currentEvent = i;
         }
 	}
-}
+} 
 
-var announce = (current_allday === null ) ? "" : [cal_events][current_allday].title;
+var announce = (currentEvent === null ) ? "" : [cal_events][currentEvent].title;
 
 if(announce.slice(0,5).toLowerCase() == 'день ') {announce = announce.slice(5);}
 
 if (announce.length > cut_length ) { announce = announce.slice(0,cut_length) + String.fromCharCode(8230);}
 
 return  announce ;
+
 
