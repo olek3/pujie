5c6b26415431ad4918e97e2429e634ed739d3cbe
OK3 <olek3a@gmail.com>
2025-04-13 23:18:12 +0530

    отладка

--- allday.js
+++ allday.js
@@ -1,8 +1,10 @@
 var current_allday = null;
 var cut_length = 18;
 var soonest_allday = [cur_evnt_full]?[cur_evnt_full].end.offset : null;
+var debug = "";
 for ( var i=0;i<[cal_events].length;i++) {
 	if ( [cal_events][i].allday && [cal_events][i].progress > 0 && [cal_events][i].progress<1 ) {
+	    debug += ":" + cal_events[i].name;
 		var offset_i = [cal_events][i].end.offset; 
         if ( soonest_allday === null || soonest_allday > offset_i ) {
             soonest_allday = offset_i;
@@ -17,5 +19,6 @@ if(announce.slice(0,5).toLowerCase() == 'день ') {announce = announce.slice(
 
 if (announce.length > cut_length ) { announce = announce.slice(0,cut_length) + String.fromCharCode(8230);}
 
+return debug
 return  announce ;
 
