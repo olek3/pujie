9b1324835531146bdd9bd4fde8575110148bef94
OK3 <olek3a@gmail.com>
2025-04-13 23:09:27 +0530

    ищем просто то, которое раньше закончится

--- allday.js
+++ allday.js
@@ -1,12 +1,11 @@
 var current_allday = null;
 var cut_length = 18;
-var latest_allday = [cur_evnt_full]?[cur_evnt_full].begin.offset : null;
+var soonest_allday = [cur_evnt_full]?[cur_evnt_full].end.offset : null;
 for ( var i=0;i<[cal_events].length;i++) {
 	if ( [cal_events][i].allday && [cal_events][i].progress > 0 && [cal_events][i].progress<1 ) {
-		var offset_i = [cal_events][i].begin.offset; 
-		var progress_i = [cal_events][i].progress;  
-        if ( latest_allday === null || latest_allday <= offset_i ||  (latest_allday = offset_i && progress_i > [cal_events][current_allday].progress ) ) {
-            latest_allday = offset_i;
+		var offset_i = [cal_events][i].end.offset; 
+        if ( soonest_allday === null || soonest_allday > offset_i ) {
+            soonest_allday = offset_i;
             current_allday = i;
         }
 	}
