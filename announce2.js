718a055017252e65fe7be28f7ba540afcf259476
OK3 <olek3a@gmail.com>
2025-05-04 19:22:17 +0530

    Revert "workaround try #4"
    This reverts commit b34f3ca8abacf59938cc7745a973c2bf30403a72.

--- announce2.js
+++ announce2.js
@@ -4,4 +4,6 @@ if([com_evnt] && [com_evnt_full] && [com_evnt_full].begin.offset >= 0 ) {
     if([cur_evnt_full].begin.offset == 0 ) return  [com_evnt_full].title + "["+[com_evnt_full].time +"]";
     return ([com_evnt_full].allday ? "{завтра}" : [com_evnt_full].begin.hour24+":"+([com_evnt_full].begin.minutes<10?"0":"")+[com_evnt_full].begin.minutes+" (+"+in_time+")" ) +"\n"+[com_evnt];
 }
-return [com_evnt];
+
+
+return "";
