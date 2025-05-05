e8fbf45a9f87125a60d6a8c392f2d369d0ad86f2
OK3 <olek3a@gmail.com>
2025-05-05 16:31:26 +0530

    reformat zero minute message

--- announce2.js
+++ announce2.js
@@ -1,7 +1,7 @@
 
 if([com_evnt] && [com_evnt_full] && [com_evnt_full].begin.offset >= 0 ) {
     var in_time = [com_evnt_full].begin.offset > 60 ? Math.round([com_evnt_full].begin.offset /6)/10+"ч" : [com_evnt_full].begin.offset +"м";
-    if([com_evnt_full].begin.offset == 0 ) return  [com_evnt_full].title + "["+[com_evnt_full].time +"]";
+    if([com_evnt_full].begin.offset == 0 ) return  "["+[com_evnt_full].time +"]\n" + [com_evnt_full].title;
     return ([com_evnt_full].allday ? "{завтра}" : [com_evnt_full].begin.hour24+":"+([com_evnt_full].begin.minutes<10?"0":"")+[com_evnt_full].begin.minutes+" (+"+in_time+")" ) +"\n"+[com_evnt];
 }
 
