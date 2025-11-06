d4389ba33e49b1da3c6fc29ae0e0786c28c18724
olek3 <olek3a@gmail.com>
2025-11-06 11:57:25 +0530

    update allday.js

--- allday.js
+++ allday.js
@@ -1,15 +1,15 @@
 var current_allday = null;
-var cut_length = 18;
+var cut_length = 13;
 var soonest_allday = [cur_evnt_full]?[cur_evnt_full].end.offset : null;
 var debug = "";
 for ( var i=0;i<[cal_events].length;i++) {
-	if ( [cal_events][i].allday && [cal_events][i].progress > 0 && [cal_events][i].progress<1 ) {
-		var offset_i = [cal_events][i].end.offset;
+        if ( [cal_events][i].allday && [cal_events][i].progress > 0 && [cal_events][i].progress<1 ) {
+                var offset_i = [cal_events][i].end.offset;
         if ( soonest_allday === null || soonest_allday >= offset_i ) {
             soonest_allday = offset_i;
             current_allday = i;
         }
-	}
+        }
 }
 
 var announce = (current_allday === null ) ? "" : [cal_events][current_allday].title;
@@ -19,4 +19,3 @@ if(announce.slice(0,5).toLowerCase() == 'день ') {announce = announce.slice(
 if (announce.length > cut_length ) { announce = announce.slice(0,cut_length) + String.fromCharCode(8230);}
 
 return  announce ;
-
